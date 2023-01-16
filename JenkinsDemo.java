7a0388937505d44193b64978608260c5b55c464e
ramakrishna300 <55428650+ramakrishna300@users.noreply.github.com>
2023-01-16 11:41:42 +0000

    Update JenkinsDemo.java

--- JenkinsDemo.java
+++ JenkinsDemo.java
@@ -1,7 +1,7 @@
 class JenkinsDemo{
-Run | Debug;
+Run|Debug
 public static void main(String args[]){
-    System.out.println("Hurray!!you are done if your output is Finished:SUCCESS") ;
+ System.out.println("Hurray!!you are done if your output is Finished:SUCCESS");
    }
 }
 
